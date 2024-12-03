1431502b178d725f4e63792b734228741548c9d0
Vy Do <vykl.do04@gmail.com>
2024-12-02 20:01:38 -0500

    move stats & progress to components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,8 @@
 </template>
 
 <script>
-import StatsCard from "@/views/StatsCard.vue";
-import ProgressBar from "@/src/ProgressBar.vue";
+import StatsCard from "@/components/StatsCard.vue";
+import ProgressBar from "@/components/ProgressBar.vue";
 
 export default {
   name: "HomeView",
